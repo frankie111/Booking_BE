42ab00c4eb3db8c514f5afb88cf5b329c5458db6
mircea <mircea.rautoiu@gmail.com>
2024-03-16 04:22:44 +0200

    scuze Oprisiule

--- routes/bookings.py
+++ routes/bookings.py
@@ -2,6 +2,8 @@ import csv
 from datetime import datetime
 
 from fastapi import APIRouter, HTTPException
+from google.cloud.firestore_v1 import FieldFilter
+from pydantic import BaseModel
 
 from firebase import get_firestore_db
 from utils.utils import convert_to_datetime
@@ -26,7 +28,24 @@ def add_locations_from_file():
             )
 
 
-def create_booking(uid: str, loc_id: str, start: datetime, end: datetime, nr_attend: int):
+class AddBookingResponse(BaseModel):
+    message: str
+
+
+class AddBookingRequest(BaseModel):
+    uid: str
+    loc_id: str
+    start: datetime
+    end: datetime
+    nr_attend: int
+
+
+@bookings.post("/bookings/", response_model=AddBookingResponse)
+def add_booking(
+        req: AddBookingRequest
+):
+    uid, loc_id, start, end, nr_attend = req
+
     db = get_firestore_db()
     loc_ref = db.collection("locations").document(loc_id)
     loc_dict = loc_ref.get()
@@ -46,42 +65,65 @@ def create_booking(uid: str, loc_id: str, start: datetime, end: datetime, nr_att
     if _type == "room" and nr_attend < cap / 2:
         raise ValueError("Meeting room cannot be less than half-booked.")
 
-    check_location_availability(loc_id, start, end)
+    overlapping_bookings = get_overlapping_bookings(loc_id, start, end)
+    if len(overlapping_bookings) > 0:
+        return
 
-    book_ref = loc_ref.collection("bookings").document()
-    book_ref.set(
-        {
-            "uid": uid,
-            "start": start,
-            "end": end,
-            "nr_attend": nr_attend
-        }
-    )
+# Raise some error or something?
+
+# book_ref = loc_ref.collection("bookings").document()
+# book_ref.set(
+#     {
+#         "uid": uid,
+#         "start": start,
+#         "end": end,
+#         "nr_attend": nr_attend
+#     }
+# )
 
 
-def check_location_availability(loc_id: str, start: datetime, end: datetime):
+# def check_location_availability(loc_id: str, start: datetime, end: datetime):
+#     db = get_firestore_db()
+#     loc_ref = db.collection("locations").document(loc_id)
+#     loc_dict = loc_ref.get()
+#
+#     if not loc_dict.exists:
+#         raise HTTPException(status_code=404, detail="Location does not exist.")
+#
+#     bookings = loc_ref.collection("bookings").stream()
+#     for booking in bookings:
+#         booking_data = booking.to_dict()
+#         booking_start = booking_data['start'].astimezone()
+#         booking_end = booking_data['end'].astimezone()
+#
+#         # Check for overlap
+#         if start < booking_end and end > booking_start:
+#             raise HTTPException(status_code=400, detail="This location is already booked at the specified time")
+
+def get_overlapping_bookings(loc_id: str, start: datetime, end: datetime):
     db = get_firestore_db()
     loc_ref = db.collection("locations").document(loc_id)
-    loc_dict = loc_ref.get()
 
-    if not loc_dict.exists:
-        raise HTTPException(status_code=404, detail="Location does not exist.")
+    # Query bookings that end after the current booking should start
+    query = loc_ref.collection("bookings").where(filter=FieldFilter("end", ">", start)).stream()
 
-    loc_dict = loc_dict.to_dict()
-    bookings = loc_ref.collection("bookings").stream()
-    for booking in bookings:
-        booking_data = booking.to_dict()
-        booking_start = booking_data['start'].astimezone()
-        booking_end = booking_data['end'].astimezone()
+    # Retrieve the active bookings
+    active_bookings = [doc.to_dict() for doc in query]
 
-        # Check for overlap
+    # Check for overlapping bookings
+    overlapping_bookings = []
+    for booking in active_bookings:
+        booking_start = booking['start'] = booking['start'].astimezone()
+        booking_end = booking['end'] = booking['end'].astimezone()
         if start < booking_end and end > booking_start:
-            raise HTTPException(status_code=400, detail="This location is already booked at the specified time")
+            overlapping_bookings.append(booking)
+
+    return overlapping_bookings
 
 
-start = convert_to_datetime("16-03-2024 11:00:00")
-end = convert_to_datetime("16-03-2024 13:00:00")
-# create_booking(
+# start = convert_to_datetime("16-03-2024 11:00:00")
+# end = convert_to_datetime("16-03-2024 13:00:00")
+# add_booking(
 #     "4FcS6HDIqPOG4hIeFrg5DO9Znwc2",
 #     "Cockpit",
 #     start,
@@ -89,4 +131,8 @@ end = convert_to_datetime("16-03-2024 13:00:00")
 #     5
 # )
 
-check_location_availability("Cockpit", start, end)
+# start = convert_to_datetime("16-03-2024 8:00:00")
+# end = convert_to_datetime("16-03-2024 9:30:00")
+# o_bookings = check_location_availability("Cockpit", start, end)
+# for booking in o_bookings:
+#     print(f"{booking["start"]} - {booking["end"]}")
